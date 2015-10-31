fcc3d6d273ff364d4dc0f6b232a07f8d9ec4a12d
kurtd04 <kurtisdavidson4@gmail.com>
2015-10-31 15:36:20 +0000

    Goals page

--- www/js/app.js
+++ www/js/app.js
@@ -38,7 +38,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     url: '/start',
     templateUrl: 'views/start.html',
   })
+  
+    .state('goal', {
+    url: '/goal',
+    templateUrl: 'views/goal.html',
+  })
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/goal');
 
 });
